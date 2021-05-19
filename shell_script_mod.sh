c71b1783727d3144f5c64e2655615fa090676649
oujisome <65988871+oujisome@users.noreply.github.com>
2021-05-19 21:52:16 +0800

    Update shell_script_mod.sh

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -22,7 +22,7 @@ function jddj(){
     # clone
     rm -rf /scripts/jddj && git clone https://hub.fastgit.org/passerby-b/JDDJ.git /scripts/jddj
     # 下载自定义cookie文件地址,如私密的gist地址,需修改
-    jddj_cookiefile="https://raw.fastgit.org/oujisome/test/master/jddj_cookie.js?token=APXOSB2DT2G62R7HGOQJCYLAUUF24"
+    jddj_cookiefile="https://raw.fastgit.org/oujisome/jdshell/main/jddj_cookie.js?token=APXOSBZLU3D6LD7BGT5NNTDAUUMCM"
     curl -so /scripts/jddj/jddj_cookie.js $jddj_cookiefile
     # 下载cookie文件失败时从备份恢复
     test $? -eq 0 || cp -rf /scripts/jddj/backup_jddj_cookie.js /scripts/backup_jddj_cookie.js
