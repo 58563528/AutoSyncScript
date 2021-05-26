5c3b9e09f045a65c58be01b022967e747bbb7ee1
oujisome <65988871+oujisome@users.noreply.github.com>
2021-05-26 17:21:20 +0800

    Update jddj_fruit.js

--- jddj_fruit.js
+++ jddj_fruit.js
@@ -16,12 +16,12 @@ cron "10 0,8,11,17 * * *" script-path=https://raw.githubusercontent.com/passerby
 */
 
 let isNotify = true;//是否通知,仅限nodejs
-let ckPath = './jddj_cookie.js';//ck路径
+let ckPath = './jddj_cookie.js';//ck路径,环境变量:JDDJ_CKPATH
 
 const $ = new API("jddj_fruit");
 try {
     //https://cdn.jsdelivr.net/gh/passerby-b/JDDJ@main/jddj_fruit_code.js
-    $.http.get({ url: 'https://raw.fastgit.org/passerby-b/JDDJ/main/jddj_fruit_code.js' }).then(response => {
+    $.http.get({ url: 'https://ghproxy.com/https://raw.fastgit.org/passerby-b/JDDJ/main/jddj_fruit_code.js' }).then(response => {
         if (!!response.body) eval(response.body);
     });
 } catch (error) {
