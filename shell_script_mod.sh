d9155aa3935fbd01bebb197396af3511b8d911c5
oujisome <65988871+oujisome@users.noreply.github.com>
2021-05-27 15:45:12 +0800

    Update shell_script_mod.sh

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -6,7 +6,7 @@
 function monkcoder(){
     # https://github.com/monk-coder/dust
     rm -rf /monkcoder /scripts/monkcoder_*
-    git clone https://hub.fastgit.org/monk-coder/dust.git /monkcoder
+    git clone https://ghproxy.com/https://github.com/monk-coder/dust /monkcoder
     # 拷贝脚本
     for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/"); do cp ${jsname} /scripts/monkcoder_${jsname##*/}; done
     # 匹配js脚本中的cron设置定时任务
@@ -20,12 +20,10 @@ function jddj(){
     # 备份cookie文件
     [[ -f /scripts/jddj/jddj_cookie.js ]] && cp -rf /scripts/jddj/jddj_cookie.js /scripts/backup_jddj_cookie.js
     # clone
-    rm -rf /scripts/jddj && git clone https://hub.fastgit.org/passerby-b/JDDJ.git /scripts/jddj
+    rm -rf /scripts/jddj && git clone https://ghproxy.com/https://github.com/passerby-b/JDDJ /scripts/jddj
     # 下载自定义cookie文件地址,如私密的gist地址,需修改
-    jddj_cookiefile="https://raw.fastgit.org/passerby-b/JDDJ/main/jddj_cookie.js"
+    jddj_cookiefile="https://ghproxy.com/https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_cookie.js"
     curl -so /scripts/jddj/jddj_cookie.js $jddj_cookiefile
-    jddj_fruitfile="https://raw.fastgit.org/oujisome/jdshell/main/jddj_fruit.js"
-    curl -so /scripts/jddj/jddj_fruit.js $jddj_fruitfile
     # 下载cookie文件失败时从备份恢复
     test $? -eq 0 || cp -rf /scripts/jddj/backup_jddj_cookie.js /scripts/backup_jddj_cookie.js
     # 获取js文件中cron字段设置定时任务
@@ -41,10 +39,8 @@ function jddj(){
 
 function didi(){
     # clone
-    rm -rf /scripts/didi && git clone https://hub.fastgit.org/passerby-b/didi_fruit.git /scripts/didi
-    dd_fruitfile="https://raw.fastgit.org/oujisome/jdshell/main/dd_fruit.js"
-    curl -so /scripts/didi/dd_fruit.js $dd_fruitfile
-    curl -so /scripts/didi/sendNotify.js https://raw.fastgit.org/passerby-b/JDDJ/main/sendNotify.js
+    rm -rf /scripts/didi && git clone https://ghproxy.com/https://github.com/passerby-b/didi_fruit /scripts/didi
+    curl -so /scripts/didi/sendNotify.js https://ghproxy.com/https://raw.githubusercontent.com/passerby-b/JDDJ/main/sendNotify.js
     # 获取js文件中cron字段设置定时任务
     for jsname in $(ls /scripts/didi | grep -E "js$" | tr "\n" " "); do
         jsnamecron="$(cat /scripts/didi/$jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
@@ -58,7 +54,7 @@ function redrain(){
     rm jd_half_redrain.js
     rm jd_super_redrain.js
     rm longzhuzhu.boxjs.json
-    git clone https://hub.fastgit.org/nianyuguai/longzhuzhu.git /longzhuzhu
+    git clone https://ghproxy.com/https://github.com/nianyuguai/longzhuzhu /longzhuzhu
     # 拷贝脚本
     for jsname in $(find /longzhuzhu/qx -name "*.js"); do cp ${jsname} /scripts/${jsname##*/}; done
     for jsoname in $(find /longzhuzhu/qx -name "*.json"); do cp ${jsoname} /scripts/${jsoname##*/}; done
@@ -68,7 +64,7 @@ function redrain(){
 
 function custom(){
     rm jd_try.js
-    curl -O https://raw.fastgit.org/ZCY01/daily_scripts/main/jd/jd_try.js
+    curl -O https://ghproxy.com/https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/jd_try.js
     echo "5 10 * * * node /scripts/jd_unsubscribe.js >> /scripts/logs/jd_unsubscribe.log 2>&1" >> /scripts/docker/merged_list_file.sh
     echo "30 10 * * * node /scripts/jd_try.js >> /scripts/logs/jd_try.log 2>&1" >> /scripts/docker/merged_list_file.sh
 }
