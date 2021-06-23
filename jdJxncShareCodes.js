145130772e7b2703eec19a09bd9a324c061343a6
ElsaKing <69370516+ElsaKing@users.noreply.github.com>
2021-06-23 12:03:13 -0500

    Update jdJxncShareCodes.js

--- jdJxncShareCodes.js
+++ jdJxncShareCodes.js
@@ -7,12 +7,14 @@
 //云服务器腾讯云函数等NOde.js用户在此处填写京京喜农场的好友码。
 // 同一个京东账号的好友助力码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
+// 注意：京喜农场 种植种子发生变化的时候，互助码也会变！！
+// 注意：京喜农场 种植种子发生变化的时候，互助码也会变！！
+// 注意：京喜农场 种植种子发生变化的时候，互助码也会变！！
+// 每个账号 shareCdoe 是一个 json，示例如下
+// {"smp":"22bdadsfaadsfadse8a","active":"jdnc_1_btorange210113_2","joinnum":"1"}
 let JxncShareCodes = [
-  '{"smp":"58cfdd77d4ff2ed345f9e5aedb8de224","active":"jdnc_1_3yuanmitao210616_2","joinnum":"3"}@{"smp":"c3872105337818ec12221f6965bdfd55","active":"jdnc_1_3yuanshousibang210601_2","joinnum":"3"}@{"smp":"d9da0248a3404803a2fd8174f3a20229","active":"jdnc_1_shanyaopian210601_2","joinnum":"3"}@{"smp":"e198a39894e21f65126eb8363f493630","active":"jdnc_1_3yuanli210616_2","joinnum":"4"}@{"smp":"dfa0c7820ba8dce0335a3399260f582b","active":"jdnc_1_3yuanmitao210616_2","joinnum":"1"}',//账号一的好友shareCode,不同好友中间用@符号隔开
-  '{"smp":"58cfdd77d4ff2ed345f9e5aedb8de224","active":"jdnc_1_3yuanmitao210616_2","joinnum":"3"}@{"smp":"c3872105337818ec12221f6965bdfd55","active":"jdnc_1_3yuanshousibang210601_2","joinnum":"3"}@{"smp":"d9da0248a3404803a2fd8174f3a20229","active":"jdnc_1_shanyaopian210601_2","joinnum":"3"}@{"smp":"e198a39894e21f65126eb8363f493630","active":"jdnc_1_3yuanli210616_2","joinnum":"4"}@{"smp":"dfa0c7820ba8dce0335a3399260f582b","active":"jdnc_1_3yuanmitao210616_2","joinnum":"1"}',//账号二的好友shareCode，不同好友中间用@符号隔开
-  '{"smp":"58cfdd77d4ff2ed345f9e5aedb8de224","active":"jdnc_1_3yuanmitao210616_2","joinnum":"3"}@{"smp":"c3872105337818ec12221f6965bdfd55","active":"jdnc_1_3yuanshousibang210601_2","joinnum":"3"}@{"smp":"d9da0248a3404803a2fd8174f3a20229","active":"jdnc_1_shanyaopian210601_2","joinnum":"3"}@{"smp":"e198a39894e21f65126eb8363f493630","active":"jdnc_1_3yuanli210616_2","joinnum":"4"}@{"smp":"dfa0c7820ba8dce0335a3399260f582b","active":"jdnc_1_3yuanmitao210616_2","joinnum":"1"}',
-  '{"smp":"58cfdd77d4ff2ed345f9e5aedb8de224","active":"jdnc_1_3yuanmitao210616_2","joinnum":"3"}@{"smp":"c3872105337818ec12221f6965bdfd55","active":"jdnc_1_3yuanshousibang210601_2","joinnum":"3"}@{"smp":"d9da0248a3404803a2fd8174f3a20229","active":"jdnc_1_shanyaopian210601_2","joinnum":"3"}@{"smp":"e198a39894e21f65126eb8363f493630","active":"jdnc_1_3yuanli210616_2","joinnum":"4"}@{"smp":"dfa0c7820ba8dce0335a3399260f582b","active":"jdnc_1_3yuanmitao210616_2","joinnum":"1"}',
-  '{"smp":"58cfdd77d4ff2ed345f9e5aedb8de224","active":"jdnc_1_3yuanmitao210616_2","joinnum":"3"}@{"smp":"c3872105337818ec12221f6965bdfd55","active":"jdnc_1_3yuanshousibang210601_2","joinnum":"3"}@{"smp":"d9da0248a3404803a2fd8174f3a20229","active":"jdnc_1_shanyaopian210601_2","joinnum":"3"}@{"smp":"e198a39894e21f65126eb8363f493630","active":"jdnc_1_3yuanli210616_2","joinnum":"4"}@{"smp":"dfa0c7820ba8dce0335a3399260f582b","active":"jdnc_1_3yuanmitao210616_2","joinnum":"1"}'
+  '',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有京喜农场助力码
 if (process.env.JXNC_SHARECODES) {
