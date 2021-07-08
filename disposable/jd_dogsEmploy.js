2c95411c5b1169bbb3de7a7ba72548075fac5f35
cdle <798731886@qq.com>
2021-07-08 15:57:41 +0800

    开工位

--- disposable/jd_dogsEmploy.js
+++ disposable/jd_dogsEmploy.js
@@ -2,7 +2,7 @@
 汪汪乐园开工位
 TG学习交流群https://t.me/cdles
 */
-const $ = Env("dogs_employ")
+const $ = Env("汪汪乐园开工位")
 const ua = `jdltapp;iPhone;3.1.0;${Math.ceil(Math.random()*4+10)}.${Math.ceil(Math.random()*4)};${randomString(40)}`
 let cookiesArr = []
 let cookie = ''
