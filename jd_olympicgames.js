0b6599727ad1c8d23562b2a8810de2eb5553c4c8
cdle <798731886@qq.com>
2021-07-08 22:18:26 +0800

    全民运动会助力

--- jd_olympicgames.js
+++ jd_olympicgames.js
@@ -2,13 +2,13 @@
 全民运动会
 更新时间：2021-7-8
 活动入口：首页右侧下方
-备注：暂时先互助，优先向前助力
+备注：暂时先互助，优先向前助力 
 1 10 * * * https://raw.githubusercontent.com/cdle/jd_study/main/jd_olympicgames.js
 */
 const $ = Env("全民运动会")
 const ua = `jdltapp;iPhone;3.1.0;${Math.ceil(Math.random()*4+10)}.${Math.ceil(Math.random()*4)};${randomString(40)}`
 let cookiesArr = []
-let cookie = ''
+let cookie = ""
 let inviters = []
 let uuid = randomString(40);
 
