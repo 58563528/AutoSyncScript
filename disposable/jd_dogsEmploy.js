79f1998d81482862e676a553336308b4e16c21df
cdle <798731886@qq.com>
2021-07-08 10:56:45 +0800

    早起福利

--- disposable/jd_dogsEmploy.js
+++ disposable/jd_dogsEmploy.js
@@ -1,8 +1,6 @@
 /*
-汪汪乐园开工位，依次助力，支持nodejs
-兼容京东jdCookie.js
-一次性脚本，用完删除
-tg学习交流群https://t.me/cdles
+汪汪乐园开工位
+TG学习交流群https://t.me/cdles
 */
 const $ = Env("dogs_employ")
 const ua = `jdltapp;iPhone;3.1.0;${Math.ceil(Math.random()*4+10)}.${Math.ceil(Math.random()*4)};${randomString(40)}`
