f521ed36085db22bd4484013c6c9239883664391
cdle <798731886@qq.com>
2021-07-31 20:08:59 +0800

    生成配置

--- jdc/models/config.go
+++ jdc/models/config.go
@@ -1,8 +1,11 @@
 package models
 
 import (
+	"io"
 	"io/ioutil"
+	"os"
 
+	"github.com/astaxie/beego/httplib"
 	"github.com/beego/beego/v2/core/logs"
 	"gopkg.in/yaml.v2"
 )
@@ -36,6 +39,24 @@ var Parallel = "parallel"
 var Config Yaml
 
 func initConfig() {
+	confDir := ExecPath + "/conf"
+	if _, err := os.Stat(confDir); err != nil {
+		os.MkdirAll(confDir, os.ModePerm)
+	}
+	for _, name := range []string{"app.conf", "config.yaml"} {
+		f, err := os.OpenFile(ExecPath+"/conf/"+name, os.O_RDWR|os.O_CREATE, 0777)
+		if err != nil {
+			logs.Warn(err)
+		}
+		s, _ := ioutil.ReadAll(f)
+		if len(s) == 0 {
+			r, err := httplib.Get("https://ghproxy.com/https://raw.githubusercontent.com/cdle/jd_study/main/jdc/conf/" + name).Response()
+			if err == nil {
+				io.Copy(f, r.Body)
+			}
+		}
+		f.Close()
+	}
 	content, err := ioutil.ReadFile(ExecPath + "/conf/config.yaml")
 	if err != nil {
 		logs.Warn("解析config.yaml读取错误: %v", err)
