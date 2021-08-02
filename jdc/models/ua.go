a0bce7053344bebd268d08ea17e1bbcc69d20e4b
cdle <798731886@qq.com>
2021-08-02 22:46:35 +0800

    更新

--- jdc/models/ua.go
+++ jdc/models/ua.go
@@ -8,12 +8,18 @@ import (
 var ua = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.14.0 main%2F1.0 baiduboxapp/11.18.0.16 (Baidu; P2 13.3.1) NABar/0.0"
 
 func initUserAgent() {
-	logs.Info("更新User-Agent")
-	var err error
-	ua, err = httplib.Get("https://ghproxy.com/https://raw.githubusercontent.com/cdle/jd_study/main/jdc/ua.txt").String()
-	if err != nil {
-		logs.Info("更新User-Agent失败")
+	if Config.UserAgent != "" {
+		logs.Info("使用自定义User-Agent")
+		ua = Config.UserAgent
+	} else {
+		logs.Info("更新User-Agent")
+		var err error
+		ua, err = httplib.Get("https://ghproxy.com/https://raw.githubusercontent.com/cdle/jd_study/main/jdc/ua.txt").String()
+		if err != nil {
+			logs.Info("更新User-Agent失败")
+		}
 	}
+
 }
 
 func GetUserAgent() string {
