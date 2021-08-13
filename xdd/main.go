4824893ca8acf88cdb9e02241c7fef6a608cc79c
cdle <798731886@qq.com>
2021-08-13 09:03:18 +0800

    x

--- xdd/main.go
+++ xdd/main.go
@@ -27,7 +27,7 @@ func main() {
 	})
 	web.Get("/", func(ctx *context.Context) {
 		if models.Config.Theme == "" {
-			models.Config.Theme = models.GhProxy + "https://raw.githubusercontent.com/xyh977732241/xdd_front_theme/master/index.html"
+			models.Config.Theme = models.GhProxy + "https://raw.githubusercontent.com/U188/web/main/random.html"
 		}
 		if theme != "" {
 			ctx.WriteString(theme)
